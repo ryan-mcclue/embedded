6adc993d05b92cb59bc59acc112b22f1e69a87b1
Ryan McClue <re.mcclue@protonmail.com>
2023-03-22 14:43:05 +1100

    jl

--- code/lcd.c
+++ code/lcd.c
@@ -71,7 +71,7 @@ enum
 };
 
 INTERNAL void
-lcd_write(LCDInfo *info, LCD_WRITE_TYPE type, u8 data)
+lcd_write(LCDInfo *info, LCD_WRITE_TYPE type, u8 data, b32 four_bits)
 {
   if (type == LCD_WRITE_TYPE_DATA)
   {
@@ -90,11 +90,28 @@ lcd_write(LCDInfo *info, LCD_WRITE_TYPE type, u8 data)
   delay_us(10);
   dio_output_set(info->e_dio, 1);
   delay_us(5);
-  // IMPORTANT(Ryan): This is where pin ordering helps  
-  info->gpio_base->ODR &= 0xFF00;
-  info->gpio_base->ODR |= data;
-  delay_us(10);
-  dio_output_set(info->e_dio, 0);
+  if (four_bits)
+  {
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= (data & 0xf0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+    delay_us(20);
+
+    dio_output_set(info->e_dio, 1);
+    delay_us(5);
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= ((data << 4) & 0xf0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+  }
+  else
+  {
+    info->gpio_base->ODR &= 0xFF00;
+    info->gpio_base->ODR |= data;
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+  }
 }
 
 INTERNAL void
@@ -116,10 +133,11 @@ lcd_s8(LCDInfo *info, u32 line_num, u32 col_num, String8 str)
 }
 
 INTERNAL LCDInfo
-init_lcd1602(LCDInit *init)
+init_lcd1602(LCDInit *init, b32 four_bit)
 {
   LCDInfo result = ZERO_STRUCT;
 
+  // TODO(Ryan): Change to 4-7
   u16 data_pin_mask = 0;
   for (u16 data_pin_i = init->data_pin_start; 
        data_pin_i < init->data_pin_start + 8; 
@@ -146,23 +164,72 @@ init_lcd1602(LCDInit *init)
 
   result.gpio_base = init->gpio_base;
 
-  HAL_Delay(20);
-  // 8bit, 2lines, 5x11 font size
-  lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x3C);
-  HAL_Delay(5);
-  // on
-  lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x0C);
-  HAL_Delay(5);
-  // clear display
-  lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x01);
-  HAL_Delay(5);
-  // put cursor at start
-  lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x02);
-  HAL_Delay(5);
+  if (four_bit)
+  {
+    // this is the initialisation phase that goes in both
+    HAL_Delay(20);
+    dio_output_set(info->rs_dio, 0);
+    dio_output_set(info->rw_dio, 0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 1);
+    delay_us(5);
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= 0x30;
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
 
-  //lcd_write(&result, LCD_WRITE_TYPE_DATA, 'a');
-  //delay_us(100);
-  //lcd_write(&result, LCD_WRITE_TYPE_DATA, 'y');
+    HAL_Delay(10);
+    dio_output_set(info->rs_dio, 0);
+    dio_output_set(info->rw_dio, 0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 1);
+    delay_us(5);
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= 0x30;
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+
+    HAL_Delay(1);
+    dio_output_set(info->rs_dio, 0);
+    dio_output_set(info->rw_dio, 0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 1);
+    delay_us(5);
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= 0x30;
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+
+    HAL_Delay(1);
+    dio_output_set(info->rs_dio, 0);
+    dio_output_set(info->rw_dio, 0);
+    delay_us(10);
+    dio_output_set(info->e_dio, 1);
+    delay_us(5);
+    info->gpio_base->ODR &= 0xFF0F;
+    info->gpio_base->ODR |= 0x20; // 4-bit mode
+    delay_us(10);
+    dio_output_set(info->e_dio, 0);
+
+    // now go on and enable 4 bit communication instruction
+
+  }
+  else
+  {
+    HAL_Delay(20);
+    // 8bit, 2lines, 5x11 font size
+    lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x3C);
+    HAL_Delay(5);
+    // on
+    lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x0C);
+    HAL_Delay(5);
+    // clear display
+    lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x01);
+    HAL_Delay(5);
+    // put cursor at start
+    lcd_write(&result, LCD_WRITE_TYPE_CMD, 0x02);
+    HAL_Delay(5);
+  }
 
   lcd_s8(&result, 0, 0, s8_lit("Ryanx"));
   delay_us(100);
